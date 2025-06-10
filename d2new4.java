daea2ab4eca7910112a5cbc141023fcf438baabb
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-06-10 11:11:28 +0530

    area of circle

---
+++ d2new4.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class d2{
+	public static void main (String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the radius of circle : ");
+		int r=sc.nextInt();
+		double ans=(3.14*r*r);
+		System.out.print("Area of circle : "+ans);
+	}
+}
