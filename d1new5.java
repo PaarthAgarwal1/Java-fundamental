a8c359b5caced257c80aa2397a830d5a41c023fb
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-06-11 11:19:20 +0530

    pens distribution

---
+++ d1new5.java
@@ -0,0 +1,9 @@
+class d1_programingElement{
+	public static void main(String args[]){
+		int numberOfPens=14;
+		int numberOfStudents=3;
+		int nonDistributedPens=14%3;
+		int pensPerStudents=14/3;
+		System.out.print("The Pen Per Students is "+pensPerStudents+" and the remaining pen not distributed is "+nonDistributedPens);
+	}
+}
