1dd33a2fb11a9bc3d8b30f9700b3480f22c1a7fd
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-06-11 11:04:42 +0530

    convert distance from km to miles

---
+++ d1new3.java
@@ -0,0 +1,7 @@
+class d1_programingElement{
+	public static void main(String args[]){
+		double distance=10.8;
+		double distanceInMiles=distance*1.6;
+		System.out.print("The distance "+distance+" km in miles is "+distanceInMiles);
+	}
+}
