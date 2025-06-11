826c518a15bd14dd315ccd5b48ac5a2cb81ceb54
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-06-11 10:51:34 +0530

    Harry's Age

---
+++ d1new1.java
@@ -0,0 +1,8 @@
+class d1_programingElement{
+	public static void main(String args[]){
+		int currYear=2024;
+		int birthYear=2000;
+		int age=currYear-birthYear;
+		System.out.print("Harry's age in 2024 is "+age);
+	}
+}
