181b9281f130f86de6d89b0bc545bc3d0795737a
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-06-11 11:00:45 +0530

    Sam's Average marks in  PCM

---
+++ d1new2.java
@@ -0,0 +1,10 @@
+class d1_programingElement{
+	public static void main(String args[]){
+		int marksMath=94;
+		int marksPhysics=95;
+		int marksChemistry=96;
+		int totalMarks=300;
+		int averagePercentage=(marksChemistry+marksMath+marksPhysics)*100/totalMarks;
+		System.out.print("Sam's average mark in PCM is "+averagePercentage);
+	}
+}
